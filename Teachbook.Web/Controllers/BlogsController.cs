31cf930a1e06a2e4e253d136f07ecb8cc228a214
SUPARNA DHARA <dhara.sup@gmail.com>
2025-09-17 01:39:58 +0530

    Displaying the blogs & tags in the home page

---
+++ Teachbook.Web/Controllers/BlogsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Teachbook.Web.Repositories;
+
+namespace Teachbook.Web.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly IBlogPostRepository blogPostRepository;
+
+        public BlogsController(IBlogPostRepository blogPostRepository)
+        {
+            this.blogPostRepository = blogPostRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index(string urlHandle)
+        {
+            var blogPost = await blogPostRepository.GetByUrlHandleAsync(urlHandle);
+            return View(blogPost);
+        }
+    }
+}
---
+++ Teachbook.Web/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Teachbook.Web.Models.Domain;
+
+namespace Teachbook.Web.Models.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+    }
+}
